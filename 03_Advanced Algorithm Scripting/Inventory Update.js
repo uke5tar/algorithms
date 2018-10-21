65652761585c79dab56882befc8f2768fe59cd34
Jannis <jannis@Janniss-MacBook-Pro.local>
2018-10-21 19:51:37 +0200

    updated task

--- 03_Advanced Algorithm Scripting/Inventory Update.js
+++ 03_Advanced Algorithm Scripting/Inventory Update.js
@@ -1,4 +1,5 @@
-/*
+/*  --- TASK ---
+
     Compare and update inventory stored in a 2d array against a second 2d array of a fresh delivery.
     Update current inventory item quantity, and if an item cannot be found,
     add the new item and quantity into the inventory array in alphabetical order.
